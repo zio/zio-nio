d2a7d404aa0fed59179f1e6264a950420d9e9388
Scala steward <scala-steward@timepit.eu>
2018-10-19 20:30:30 +0200

    Update scalaz-zio to 0.3.1

--- project/ScalazBuild.scala
+++ project/ScalazBuild.scala
@@ -15,7 +15,7 @@ object Scalaz {
 
   val compileAndTest = Seq(
     "org.scalaz" %% "scalaz-core" % scalazVersion % "compile, test",
-    "org.scalaz" %% "scalaz-zio"  % "0.3.0"
+    "org.scalaz" %% "scalaz-zio"  % "0.3.1"
   )
 
   private val stdOptions = Seq(
