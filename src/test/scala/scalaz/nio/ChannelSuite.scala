d8cb9944d2a82ccb122947f6f48b4e66e462bd4f
Yuriy Susuk <yuriy.susuk@scout24.com>
2018-10-23 15:23:34 +0200

    fmt

--- src/test/scala/scalaz/nio/ChannelSuite.scala
+++ src/test/scala/scalaz/nio/ChannelSuite.scala
@@ -13,18 +13,18 @@ object ChannelSuite extends RTS {
     section(
       test("read/write") { () =>
         val testProgram: IO[Exception, Boolean] = for {
-          src          <- Buffer.byte(3)
-          sink         <- Buffer.byte(3)
-          address      <- IO.now(new InetSocketAddress(1337))
-          server       <- AsynchronousServerSocketChannel()
-          _            <- server.bind(address)
-          client       <- AsynchronousSocketChannel()
-          _            <- client.connect(address)
-          nSrc         <- client.write(src)
+          src     <- Buffer.byte(3)
+          sink    <- Buffer.byte(3)
+          address <- IO.now(new InetSocketAddress(1337))
+          server  <- AsynchronousServerSocketChannel()
+          _       <- server.bind(address)
+          client  <- AsynchronousSocketChannel()
+          _       <- client.connect(address)
+          nSrc    <- client.write(src)
           // accept should be called after something is written int src channel
           // how to consiniously accept writes?
-          worker       <- server.accept
-          nSink        <- worker.read(sink)
+          worker <- server.accept
+          nSink  <- worker.read(sink)
         } yield nSrc == nSink
 
         assert(unsafeRun(testProgram))
