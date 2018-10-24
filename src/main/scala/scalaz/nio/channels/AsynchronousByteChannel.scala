42b5998077b3987fa18e6fc9928ce2be4dc112de
Alessandro Zoffoli <alessandro.zoffoli@gmail.com>
2018-10-24 16:25:20 +0200

    Add missing methods for channels

--- src/main/scala/scalaz/nio/channels/AsynchronousByteChannel.scala
+++ src/main/scala/scalaz/nio/channels/AsynchronousByteChannel.scala
@@ -1,5 +1,6 @@
 package scalaz.nio.channels
 
+import java.lang.{ Integer => JInteger }
 import java.nio.channels.{ AsynchronousByteChannel => JAsynchronousByteChannel }
 
 import scalaz.nio.ByteBuffer
@@ -13,16 +14,25 @@ class AsynchronousByteChannel(private val channel: JAsynchronousByteChannel) {
    *  read, or -1 if no bytes were read.
    */
   final def read(b: ByteBuffer): IO[Exception, Int] =
-    wrap[Integer] { h =>
-      channel.read(b.buffer, (), h)
-    }.map(_.toInt)
+    wrap[Unit, JInteger](h => channel.read(b.buffer, (), h)).map(_.toInt)
+
+  /**
+   *  Reads data from this channel into buffer, returning the number of bytes
+   *  read, or -1 if no bytes were read.
+   */
+  final def read[A](b: ByteBuffer, attachment: A): IO[Exception, Int] =
+    wrap[A, JInteger](h => channel.read(b.buffer, attachment, h)).map(_.toInt)
 
   /**
    *  Writes data into this channel from buffer, returning the number of bytes written.
    */
   final def write(b: ByteBuffer): IO[Exception, Int] =
-    wrap[Integer] { h =>
-      channel.write(b.buffer, (), h)
-    }.map(_.toInt)
+    wrap[Unit, JInteger](h => channel.write(b.buffer, (), h)).map(_.toInt)
+
+  /**
+   *  Writes data into this channel from buffer, returning the number of bytes written.
+   */
+  final def write[A](b: ByteBuffer, attachment: A): IO[Exception, Int] =
+    wrap[A, JInteger](h => channel.write(b.buffer, attachment, h)).map(_.toInt)
 
 }
--- src/main/scala/scalaz/nio/channels/IOAsyncUtil.scala
+++ src/main/scala/scalaz/nio/channels/IOAsyncUtil.scala
@@ -6,13 +6,13 @@ import scalaz.zio.{ Async, ExitResult, IO }
 
 object IOAsyncUtil {
 
-  def wrap[T](op: JCompletionHandler[T, Unit] => Unit): IO[Exception, T] =
+  def wrap[A, T](op: JCompletionHandler[T, A] => Unit): IO[Exception, T] =
     IO.async0[Exception, T] { k =>
-      val handler = new JCompletionHandler[T, Unit] {
-        def completed(result: T, u: Unit): Unit =
+      val handler = new JCompletionHandler[T, A] {
+        def completed(result: T, u: A): Unit =
           k(ExitResult.Completed(result))
 
-        def failed(t: Throwable, u: Unit): Unit =
+        def failed(t: Throwable, u: A): Unit =
           t match {
             case e: Exception => k(ExitResult.Failed(e))
             case _            => k(ExitResult.Terminated(List(t)))
