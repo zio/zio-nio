d2703d351a02f47b8d87110b3f22a3c96a475d16
Yuriy Susuk <yuriy.susuk@scout24.com>
2018-11-16 16:11:22 +0100

    get[T] and get[T](index) syntax added

--- src/main/scala/scalaz/nio/Buffer.scala
+++ src/main/scala/scalaz/nio/Buffer.scala
@@ -3,7 +3,7 @@ package scalaz.nio
 import scalaz.zio.IO
 //import scalaz.Scalaz._
 
-import java.nio.{ Buffer => JBuffer, ByteBuffer => JByteBuffer }
+import java.nio.{ Buffer => JBuffer, ByteBuffer => JByteBuffer, CharBuffer => JCharBuffer }
 
 import scala.reflect.ClassTag
 //import scala.{Array => SArray}
@@ -52,33 +52,103 @@ abstract class Buffer[A: ClassTag, B <: JBuffer] private[nio] (private[nio] val 
 
 }
 
+object Buffer {
+  def byte(capacity: Int) = ByteBuffer(capacity)
+
+  def char(capacity: Int) = CharBuffer(capacity)
+}
+
+// ToDo: In current implementation only non-direct buffers are supported
 class ByteBuffer private (val byteBuffer: JByteBuffer)
     extends Buffer[Byte, JByteBuffer](byteBuffer) {
   def array: IO[Exception, Array[Byte]] = IO.syncException(byteBuffer.array())
+
+  def asCharBuffer: IO[Exception, CharBuffer] = IO.syncException(CharBuffer(byteBuffer.asCharBuffer()))
+
+  def asReadOnlyBuffer: IO[Exception, ByteBuffer] = IO.syncException(ByteBuffer(byteBuffer.asReadOnlyBuffer()))
+
+  def duplicate: IO[Exception, ByteBuffer] = IO.syncException(ByteBuffer(byteBuffer.duplicate()))
+
+  def get: IO[Exception, Byte] = IO.syncException(byteBuffer.get())
+
+  // ToDo: java api returns this buffer, scalaz api returns Unit to emphaisze that this operation has side-effect (e.g. transering bytes into dst, changing buffer's position)
+  def get(dst: Array[Byte]): IO[Exception, Unit] = IO.syncException(byteBuffer.get(dst)).void
+
+  def get(dst: Array[Byte], offset: Int, length: Int): IO[Exception, Unit] = IO.syncException(byteBuffer.get(dst, offset, length)).void
+
+  def get(index: Int): IO[Exception, Byte] = IO.syncException(byteBuffer.get(index))
+
+  def get[T](implicit read: Read[T]): IO[Exception, T] = IO.syncException(read.value)
+
+  def get[T](index: Int)(implicit readIndex: ReadIndex[T]): IO[Exception, T] = IO.syncException(readIndex.value(index))
+
+  trait Read[T] {
+    def value: T
+  }
+
+  trait ReadIndex[T] {
+    def value(index: Int): T
+  }
+
+  implicit val charReader: Read[Char] = new Read[Char] {
+    def value = byteBuffer.getChar()
+  }
+
+  implicit val doubleReader: Read[Double] = new Read[Double] {
+    def value = byteBuffer.getDouble()
+  }
+
+  implicit val charIndexReader: ReadIndex[Char] = new ReadIndex[Char] {
+    def value(index: Int) = byteBuffer.getChar(index)
+  }
+
+  implicit val doubleIndexReader: ReadIndex[Double] = new ReadIndex[Double] {
+    def value(index: Int) = byteBuffer.getDouble(index)
+  }
+
+// Absolute get method for reading a char value.
+// abstract DoublegetDouble()
+// Relative get method for reading a double value.
+// abstract DoublegetDouble(int index)
+// Absolute get method for reading a double value.
+// abstract FloatgetFloat()
+// Relative get method for reading a float value.
+// abstract FloatgetFloat(int index)
+// Absolute get method for reading a float value.
+// abstract IntgetInt()
+// Relative get method for reading an int value.
+// abstract IntgetInt(int index)
+// Absolute get method for reading an int value.
+// abstract LonggetLong()
+// Relative get method for reading a long value.
+// abstract LonggetLong(int index)
+// Absolute get method for reading a long value.
+// abstract ShortgetShort()
+// Relative get method for reading a short value.
+// abstract ShortgetShort(int index)
+// Absolute get method for reading a short value.
+
 }
 
 object ByteBuffer {
+
+  private[nio] def apply (charBuffer: JByteBuffer): ByteBuffer = new ByteBuffer(charBuffer)
 
   def apply(capacity: Int): IO[Exception, ByteBuffer] =
     IO.syncException(JByteBuffer.allocate(capacity)).map(new ByteBuffer(_))
 }
 
-object Buffer {
-  def byte(capacity: Int) = ByteBuffer(capacity)
+class CharBuffer private (val charBuffer: JCharBuffer)
+    extends Buffer[Char, JCharBuffer](charBuffer) {
 
-  def char(capacity: Int) = CharBuffer(capacity)
+  def array: IO[Exception, Array[Char]] =
+    IO.syncException(charBuffer.array().asInstanceOf[Array[Char]])
+}
 
-  private class CharBuffer private (private val charBuffer: JByteBuffer)
-      extends Buffer[Char, JByteBuffer](charBuffer) {
+object CharBuffer {
 
-    def array: IO[Exception, Array[Char]] =
-      IO.syncException(charBuffer.array().asInstanceOf[Array[Char]])
-  }
+  private[nio] def apply (charBuffer: JCharBuffer): CharBuffer = new CharBuffer(charBuffer)
 
-  private object CharBuffer {
-
-    def apply(capacity: Int): Buffer[Char, JByteBuffer] =
-      new CharBuffer(JByteBuffer.allocate(capacity))
-  }
-
+  def apply(capacity: Int): IO[Exception, CharBuffer] =
+    IO.syncException(JCharBuffer.allocate(capacity)).map(new CharBuffer(_))
 }
