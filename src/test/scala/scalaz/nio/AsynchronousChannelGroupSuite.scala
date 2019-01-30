7f753688eef70a79be4a5e4fc86d5be01356d391
Arkadiusz Kaczynski <arek@scalac.io>
2019-01-30 20:31:47 +0100

    #44 it should be channel group not channel

--- src/test/scala/scalaz/nio/AsynchronousChannelGroupSuite.scala
+++ src/test/scala/scalaz/nio/AsynchronousChannelGroupSuite.scala
@@ -1,11 +1,29 @@
 package scalaz.nio
 import scalaz.zio.RTS
+import testz.{Harness, ResourceHarness, assert}
+import java.nio.channels.{AsynchronousChannelGroup => JAsynchronousChannelGroup}
 
-import testz.{Harness, assert}
+import org.scalamock.scalatest.MockFactory
+import scalaz.nio.channels.AsynchronousChannelGroup
 
-object AsynchronousChannelGroupSuite extends RTS{
-  def tests[T](harness: Harness[T]): T = {
+object AsynchronousChannelGroupSuite extends RTS with MockFactory{
+  class Fixture {
+    val jGroupMock = mock[JAsynchronousChannelGroup]
+    val testObj = new AsynchronousChannelGroup(jGroupMock)
+  }
+
+  def tests[T](harness: ResourceHarness[T]): T = {
     import harness._
+    section(
+      namedSection("awaitTermination") {
+        allocate(() => List(1))(test("fails") { case (list, _) =>
 
+          assert(1 == 1)
+        })
+        test("succeeds") { () =>
+
+        }
+      }
+    )
   }
 }
