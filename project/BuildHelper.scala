b470a2079f3ed2ecba54dab919d67bd18da66c94
Lachlan O'Dea <lodea@mac.com>
2019-11-09 18:05:10 +1100

    Upgrade to Scala 2.13.1.

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -23,7 +23,7 @@ object BuildHelper {
 
   private val SilencerVersion = "1.4.4"
   private val Scala212        = "2.12.10"
-  private val Scala213        = "2.13.0"
+  private val Scala213        = "2.13.1"
 
   private val stdOptions = Seq(
     "-encoding",
