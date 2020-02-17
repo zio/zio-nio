303ac3d1d920c6bb07cb2e6dc1b1738176b4e1cf
Scala Steward <me@scala-steward.org>
2020-02-17 15:24:36 +0100

    Update silencer-lib, silencer-plugin to 1.5.0

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -22,7 +22,7 @@ object BuildHelper {
   val ZioCoreVersion = "1.0.0-RC17"
   val ZioInteropJava = "1.1.0.0-RC6"
 
-  private val SilencerVersion = "1.4.4"
+  private val SilencerVersion = "1.5.0"
   private val Scala212        = "2.12.10"
   private val Scala213        = "2.13.1"
 
