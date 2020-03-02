24aad493c823f45152cedc6b5fbe2e1e0bb27e28
Vasil Vasilev <vasilv@protonmail.com>
2020-03-02 04:21:35 +0100

    DatagramChannel APIs and scaladoc (#155)

--- nio/src/main/scala/zio/nio/channels/ScatteringByteChannel.scala
+++ nio/src/main/scala/zio/nio/channels/ScatteringByteChannel.scala
@@ -5,7 +5,7 @@ package channels
 import java.nio.channels.{ ScatteringByteChannel => JScatteringByteChannel }
 import java.nio.{ ByteBuffer => JByteBuffer }
 
-import zio.{ Chunk, IO }
+import zio.{ Chunk, IO, UIO }
 import zio.nio.core.Buffer
 
 trait ScatteringByteChannel extends Channel {
@@ -16,15 +16,13 @@ trait ScatteringByteChannel extends Channel {
 
   final def readBuffer(dst: Buffer[Byte]): IO[Exception, Option[Long]] = readBuffer(List(dst))
 
-  final def read(capacity: Int): IO[Exception, Option[Chunk[Byte]]] = {
-    require(capacity > 0)
+  final def read(capacity: Int): IO[Exception, Chunk[Byte]] =
     for {
       buffer    <- Buffer.byte(capacity)
       readCount <- readBuffer(buffer)
       _         <- buffer.flip
-      chunk     <- readCount.map(count => buffer.getChunk(count.toInt).map(Some.apply)).getOrElse(IO.succeed(None))
+      chunk     <- readCount.map(count => buffer.getChunk(count.toInt)).getOrElse(UIO.succeed(Chunk.empty))
     } yield chunk
-  }
 
   private def unwrap(dsts: List[Buffer[Byte]]): Array[JByteBuffer] =
     dsts.map(d => d.buffer.asInstanceOf[JByteBuffer]).toList.toArray
