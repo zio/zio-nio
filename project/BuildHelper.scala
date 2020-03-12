b13961cc3686c3e1ba4b1944b4fd73320189add4
Scala Steward <me@scala-steward.org>
2020-03-12 03:08:38 +0100

    Update zio, zio-streams, zio-test, ... to 1.0.0-RC18-2

--- project/BuildHelper.scala
+++ project/BuildHelper.scala
@@ -19,7 +19,7 @@ object BuildHelper {
     incOptions ~= (_.withLogRecompileOnMacro(false))
   )
 
-  val ZioCoreVersion = "1.0.0-RC18"
+  val ZioCoreVersion = "1.0.0-RC18-2"
 
   private val SilencerVersion = "1.6.0"
   private val Scala212        = "2.12.10"
