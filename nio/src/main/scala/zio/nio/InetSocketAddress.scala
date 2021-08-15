e118408323fb6c391240f1cf731539f3882cc2b7
Steven Vroonland <steven@dataworkz.nl>
2021-08-15 10:51:45 +0200

    Fix name conflict

--- nio/src/main/scala/zio/nio/InetSocketAddress.scala
+++ nio/src/main/scala/zio/nio/InetSocketAddress.scala
@@ -113,7 +113,7 @@ object InetSocketAddress {
    * This method will attempt to resolve the hostname; if this fails, the returned
    * socket address will be ''unresolved''.
    */
-  def hostname(hostname: String, port: Int): UIO[InetSocketAddress] =
+  def hostName(hostname: String, port: Int): UIO[InetSocketAddress] =
     UIO.effectTotal(new InetSocketAddress(new JInetSocketAddress(hostname, port)))
 
   /**
@@ -130,7 +130,7 @@ object InetSocketAddress {
    * This method will attempt to resolve the hostname; if this fails, the returned
    * socket address will be ''unresolved''.
    */
-  def hostnameEphemeral(hostname: String): UIO[InetSocketAddress] = this.hostname(hostname, 0)
+  def hostnameEphemeral(hostname: String): UIO[InetSocketAddress] = this.hostName(hostname, 0)
 
   /**
    * Creates a resolved socket address from a hostname, with an ephemeral port.
