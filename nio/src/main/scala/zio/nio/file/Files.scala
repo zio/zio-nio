5a6eaf355c1314f89a5a799b2e9fa2540428b869
Hannes Bibel <hannes.bibel@gmail.com>
2023-03-29 21:33:32 +0200

    #586 - Delete inner directories first in deleteRecursive

--- nio/src/main/scala/zio/nio/file/Files.scala
+++ nio/src/main/scala/zio/nio/file/Files.scala
@@ -21,6 +21,7 @@ import java.nio.file.{
 import java.util.function.BiPredicate
 import scala.jdk.CollectionConverters._
 import scala.reflect._
+import zio.ZIOAspect
 
 object Files {
 
@@ -134,7 +135,13 @@ object Files {
     attemptBlocking(JFiles.deleteIfExists(path.javaPath)).refineToOrDie[IOException]
 
   def deleteRecursive(path: Path)(implicit trace: Trace): ZIO[Any, IOException, Long] =
-    newDirectoryStream(path).mapZIO(delete).run(ZSink.count) <* delete(path)
+    newDirectoryStream(path).mapZIO { p =>
+      for {
+        deletedInSubDirectory <- deleteRecursive(p).whenZIO(isDirectory(p)).map(_.getOrElse(0L))
+        deletedFile           <- deleteIfExists(p).map(if (_) 1 else 0)
+      } yield deletedInSubDirectory + deletedFile
+    }
+      .run(ZSink.sum) <* delete(path)
 
   def copy(source: Path, target: Path, copyOptions: CopyOption*)(implicit
     trace: Trace
